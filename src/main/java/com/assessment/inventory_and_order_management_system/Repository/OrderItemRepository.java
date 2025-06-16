922b31b0c5ec2d88e4e410a73a779b306b0d6f27
soumyadeep dey <105117395+soumyadeepdey2003@users.noreply.github.com>
2025-06-17 01:18:23 +0530

    java commit

--- src/main/java/com/assessment/inventory_and_order_management_system/Repository/OrderItemRepository.java
+++ src/main/java/com/assessment/inventory_and_order_management_system/Repository/OrderItemRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
     List<OrderItem> findByItemId(Long itemId);
-    List<OrderItem> findByOrderId(Long orderId);
+
 
 }
